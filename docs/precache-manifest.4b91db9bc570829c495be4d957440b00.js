2d19613f649fd2119df35a676dc1427a22801842
mikfrei <mikefreiman.now@gmail.com>
2019-06-22 19:53:22 -0500

    Added /docs

---
+++ docs/precache-manifest.4b91db9bc570829c495be4d957440b00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e2f4d7e1b0be6cf279d8678b77b89b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22cd00e9069bae75bd33",
+    "url": "/static/js/2.188d31dd.chunk.js"
+  },
+  {
+    "revision": "3a0cc90fb849384e6933",
+    "url": "/static/js/main.f9fb8ba6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8bffcc754e9874d3ffba35de38e3898f",
+    "url": "/static/media/tile.8bffcc75.svg"
+  }
+]);
